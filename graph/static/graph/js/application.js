4d190007f4e6f19779860d6ceda74f4b3e1c9759
Daniel Matthews <dr.dan.matthews@gmail.com>
2015-07-04 11:02:56 +1000

    removed graph

--- graph/static/graph/js/application.js
+++
@@ -1,7 +0,0 @@
-$(function(){	
-	var Ann = new AnnotationModel();
-	var Graph = new GraphModel();
-
-	new AnnotationView({ model: Ann, graphModel: Graph });
-	new GraphPlotView({ model: Graph });
-});
